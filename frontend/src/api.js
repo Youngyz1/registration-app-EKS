60a973a3c70b701b9d8d6fcb6df9fa03e25315b8
Youngyz1 <uche2net@gmail.com>
2026-03-07 19:22:11 +0100

    fix: pin nginx alpine, upgrade packages to fix CVEs, use relative /api baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // http://backend:8000/api
+  baseURL: process.env.REACT_APP_API_URL || "/api",
   headers: { "Content-Type": "application/json" },
 });
 
@@ -12,3 +12,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
