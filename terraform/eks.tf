9f3ec66824e809bba12c5e0879bfcfaee73c9307
Youngyz1 <uche2net@gmail.com>
2026-03-09 13:08:06 +0100

    fix: add ServiceAccount to Helm chart so it is created automatically on install

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,4 +1,4 @@
-﻿resource "aws_iam_role" "eks_cluster" {
+resource "aws_iam_role" "eks_cluster" {
   name = "${var.cluster_name}-cluster-role"
 
   assume_role_policy = jsonencode({
@@ -143,8 +143,41 @@ resource "aws_eks_access_policy_association" "github_actions" {
 }
 # EBS CSI Driver addon
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name   = "aws-ebs-csi-driver"
+  cluster_name             = aws_eks_cluster.main.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
 
-  depends_on = [aws_eks_node_group.main]
+  depends_on = [
+    aws_eks_node_group.main,
+    aws_iam_role_policy_attachment.ebs_csi_driver,
+  ]
 }
+
+# EBS CSI Driver IAM Role (IRSA)
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "${var.cluster_name}-ebs-csi-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")}:aud" = "sts.amazonaws.com"
+          "${replace(aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  role       = aws_iam_role.ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+
